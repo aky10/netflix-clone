9fa95b855e63034e371d91846f63b69f33fa4117
Your Name <contactamity8@gmail.com>
2024-01-16 13:26:01 +0530

    do

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -17,5 +17,3 @@ export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
 
 export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_medium.jpg";
-
-export const OPENAI_KEY = process.env.REACT_APP_API_KEY;
